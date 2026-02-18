7b5e49247d179e33def60b71729a7328f5d795df
Raphael Ho <raphaeldominique@gmail.com>
2026-02-18 01:14:51 -0700

    Adding next page functionality

--- kijiji.py
+++ kijiji.py
@@ -13,89 +13,85 @@ import re
 DRIVER = None
 
 KIJIJI_URL = "https://www.kijiji.ca/b-cars-trucks/edmonton/c174l1700203?kilometers=0__150000&price=0__15000&transmission=1&view=list"
+NEXT_BUTTON_XPATH = "/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[1]/div[2]/div[3]/div/div/div[3]/div/div/nav/ul/li[3]/a"
+NEXT_BUTTON_REL_XPATH = '//*[@id="base-layout-main-wrapper"]/div/div[3]/div[2]/div[1]/div[2]/div[3]/div/div/div[3]/div/div/nav/ul/li[3]/a'
+NEXT_BUTTON_CLASS = "sc-c8742e84-0 jwUdte sc-4c795659-3 garPwt"
 LISTING_LIST_CLASS = "sc-31c99dfc-0 dREgHU"
-LISTING_LIST_XPATH = '/html/body/div[1]/div/div/div/main/div/div[3]/div[2]/div[1]/div[2]/div[3]/div/div/ul'
-
 TITLE_CLASS = "sc-eb5dea32-1 gPyerW"
-
 PRICE_CLASS = "sc-991ea11d-0 eZUULr sc-54de28bc-3 imlMOc"
-
 ODOMETER_CLASS = "sc-991ea11d-0 epsmyv sc-4b5a8895-2 eEvVV"
-
 TRANSMISSION_CLASS = "sc-991ea11d-0 epsmyv sc-4b5a8895-2 eEvVV"
 
 
-def traverse():
-    with open("source.txt", "w") as file:
-        file.write(DRIVER.page_source)
-    
-    listings = []
+def traverse(listings):
+    time.sleep(3)       # wait for listings to load
 
-    html_content = DRIVER.page_source
-    soup = BeautifulSoup(html_content, "html.parser")
-    
-    time.sleep(5)       # wait for listings to load
+    list = SOUP.find("ul", class_=LISTING_LIST_CLASS)
 
-    list = soup.find("ul", class_=LISTING_LIST_CLASS)
 
     try:
-        listings = list.find_all("li")
+        all_listings = list.find_all("li")
     except Exception as e:
         print(f"{e}, Listings could not be found.")
 
-    with open ("titles.txt", "w") as file:
         
-        for post in listings:
+    for post in all_listings:
+        
+        try:        # Title
+            title = post.find("a", class_=TITLE_CLASS)
+            title_string = title.text
+        except AttributeError as e:
+            # print(f"{e}. Title could not be found.")
+            continue
+
+        try:        # Price
+            price = post.find("p", class_=PRICE_CLASS)
+            price_string = price.text
+        except Exception as e:
+            print(f"{e}. Price finding error.")
+            continue
             
-            try:        # Title
-                title = post.find("a", class_=TITLE_CLASS)
-                file.write(f"{title.text}|")
-            except Exception as e:
-                print(f"{e}. Title could not be found.")
-                continue
+        try:        # Kilometers
+            odometer = post.find("p", class_=ODOMETER_CLASS)
+            odometer_string = odometer.text
+            space_index = odometer_string.find(" ")
+            corrected_odometer = int(odometer_string[:space_index])
+            if (corrected_odometer < 1000):
+                corrected_odometer = corrected_odometer * 1000
+            correct = str(corrected_odometer) + ' km'
+        except Exception as e:
+            print(f"{e}. Odometer finding error.")
+            continue
 
-            try:        # Price
-                price = post.find("p", class_=PRICE_CLASS)
-                file.write(f"{title.text}|")
-            except Exception as e:
-                print(f"{e}. Price finding error.")
-                continue
-                
-            try:        # Kilometers
-                odometer = post.find("p", class_=ODOMETER_CLASS)
-                file.write(f"{odometer.text}|")
-            except Exception as e:
-                print(f"{e}. Odometer finding error.")
-                continue
+        try:        # Transmission type
+            transmission = odometer.find_next("p", class_=TRANSMISSION_CLASS)
+            transmission_string = transmission.text
+        except Exception as e:
+            print(f"{e}. Transmission finding error.")
+            continue
 
-            try:        # Transmission type
-                transmission = odometer.find_next("p", class_=TRANSMISSION_CLASS)
-                file.write(f"{transmission.text}|")
-            except Exception as e:
-                print(f"{e}. Transmission finding error.")
-                continue
+        try:        # Link is found in title
+            link = title.get("href")
+        except Exception as e:
+            print(f"{e}. Link error.")
+            continue
+        
+        # Regex expression for year
+        try:
+            year = re.search("\\d{4}", title.text)
+            if year:
+                parsed_year = year.group()
+            else:
+                parsed_year = "NA"
+        except Exception as e:
+            print(f"{e}. Year parsing error.")
 
-            try:        # Link is found in title
-                link = title.get("href")
-                file.write(f"{link}\n")
-            except Exception as e:
-                print(f"{e}. Link error.")
-                continue
-            
-            year = re.search("\\d{4}", title)
-            print(year.group())
-            listings.append({"Title:": title.text, "Price":price.text, "Kilometers":odometer.text, "Transmission:":transmission.text,"Year": year ,"Link":link})
 
-    # with open ("dict.txt", "w") as file:
-    for item in listings:
-        print(item)
-
-    print("Finished")
+        listings.append({"Title": title_string, "Price": price_string, "Kilometers": correct, "Transmission": transmission_string, "Year": parsed_year ,"Link":link})
     
-    
-    
+
 def main():
-    global DRIVER, WAIT
+    global DRIVER, WAIT, SOUP
     open_options = Options()
 
     # Opens page until click close yourself, can delete later
@@ -109,7 +105,27 @@ def main():
     DRIVER = webdriver.Chrome(options=open_options)
     WAIT = WebDriverWait(DRIVER, 20)
     DRIVER.get(KIJIJI_URL)
-    traverse()
+
+    html_content = DRIVER.page_source
+    SOUP = BeautifulSoup(html_content, "html.parser")
+
+    listings = []
+
+    time.sleep(3)
+    
+    next = SOUP.find("a", class_=NEXT_BUTTON_CLASS)
+    while (True):
+        traverse(listings)
+        if (next is not None):
+            next.click()
+        else:
+            break
+
+    with open ("dict.txt", "w") as file:
+        for item in listings:
+            file.write(f"{item["Title"]}|{item["Price"]}|{item["Kilometers"]}|{item["Transmission"]}|{item["Year"]}|{item["Link"]}\n")
+
+    print("Finished")
 
 if __name__ == "__main__":
     main()
